6501750c881cb2cefa16290ee7a90004704d5b77
Full Frontal <autodesk1685@gmail.com>
2019-03-08 15:21:47 -0500

    completed files for crystal game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,104 @@
-//This code does not work.
-//I wrote it out the best I could.
+$(document).ready(function() {
+    var randomNumber = Math.floor(Math.random() * 101) + 1;
+    $("#number-to-beat").html(randomNumber);
+    console.log(randomNumber);
 
-$(document).ready()
+    var crystal1 = Math.floor(Math.random() * 10) + 1;
+    var crystal2 = Math.floor(Math.random() * 10) + 1;
+    var crystal3 = Math.floor(Math.random() * 10) + 1;
+    var crystal4 = Math.floor(Math.random() * 10) + 1;
 
-var displayedNumber = "";
-var wins = 0;
-var losses =0;
-var totalNumber = 0;
-var images = "";
+    var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
 
+    $("#total-wins").text(wins);
+    $("#total-losses").text(losses);
 
-//still having issues with math.random.. shows undefined.
-var randomDisplayedNumber = math.floor(math.random() * 99) + 1;
- console.log(randomDisplayedNumber);
+function gameReset() {
+    var randomNumber = Math.floor(Math.random() * 101) + 1;
+    $("#number-to-beat").text(randomNumber);
+    var crystal1 = Math.floor(Math.random() * 10) + 1;
+    var crystal2 = Math.floor(Math.random() * 10) + 1;
+    var crystal3 = Math.floor(Math.random() * 10) + 1;
+    var crystal4 = Math.floor(Math.random() * 10) + 1;
+    console.log(randomNumber);
+    totalScore = 0;
+    $("#total-score").text(totalScore);
+}
 
-$(".displayedNumber").text(randomDisplayedNumber);
+function youLoose() {
+    alert("You Lose!");
+    losses++;
+    $("#total-losses").text(losses);
+    gameReset();
 
+}
 
-// I think this is the code for clicking on an item.
+function youWin() {
+    alert("You Won!");
+    wins++;
+    $("#total-wins").text(wins);
+    gameReset();
+}
 
-$(".crystalImages").on("click", function() {
-    var imageCrystal = userInput;
+$("#crystal-image-1").on("click", function() { totalScore = totalScore + crystal1; 
+    $("#total-score").text(totalScore);
+    console.log(totalScore);
+    if(totalScore == randomNumber) {
+         youWin();
+    } else if (totalScore > randomNumber) { 
+         youLoose();
 
-    if (userInput === randomDisplayedNumber) {
-        alert("You Win!");
     }
+});
 
-    else (userInput == randomDisplayedNumber) {
-        alert("You Lose!");
+$("#crystal-image-2").on("click", function() { totalScore = totalScore + crystal2;
+    $("#total-score").text(totalScore);
+    console.log(totalScore);
+    if(totalScore == randomNumber) {
+         youWin();
+    } else if (totalScore > randomNumber) { 
+         youLoose();
+
+    }
+});
+
+$("#crystal-image-3").on("click", function() { totalScore = totalScore + crystal3;
+    $("#total-score").text(totalScore);
+    console.log(totalScore);
+    if(totalScore == randomNumber) {
+        youWin();
+    } else if (totalScore > randomNumber) { 
+        youLoose();
+
+    }
+});
+
+$("#crystal-image-4").on("click", function() { totalScore = totalScore + crystal4;
+    $("#total-score").text(totalScore);
+    console.log(totalScore);
+    if(totalScore == randomNumber) {
+         youWin();
+    } else if (totalScore > randomNumber) { 
+        youLoose();
+
     }
 });
 
 
-//Also creating a random number for the crystal
 
-var crystal1 = math.floor(math.random() * 10 + 1);
-var crystal2 = math.floor(math.random() * 10 + 1);
-var crystal3 = math.floor(math.random() * 10 + 1);
-var crystal4 = math.floor(math.random() * 10 + 1);
+});
 
-// Not really sure on how to put it all together.
 
-// There should some functions some where to be decleared and called. 
+
+
+
+
+
+
+
+
 
 
  
